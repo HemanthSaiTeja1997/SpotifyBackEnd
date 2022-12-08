1c5e45a8587217275b4ea6acbfbdfd3e0916038b
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-12-08 19:21:31 +0530

    added the code with EurekaServerApplication

--- bej-c2-s3-restful-mongodb-challange-track-master/src/main/java/com/example/BejC2S3RestfulMongoDbChallengeTrackApplication.java
+++ bej-c2-s3-restful-mongodb-challange-track-master/src/main/java/com/example/BejC2S3RestfulMongoDbChallengeTrackApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BejC2S3RestfulMongoDbChallengeTrackApplication {
 
     public static void main(String[] args) {
